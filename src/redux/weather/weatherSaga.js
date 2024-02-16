7e6070207f6315e595373cabc7785347b23bd86e
maliha shahi <maliha.shahi.03@gmail.com>
2024-02-16 22:25:28 +0330

    takeLaset action and multiple wacther at redux_saga

--- src/redux/weather/weatherSaga.js
+++ src/redux/weather/weatherSaga.js
@@ -1,22 +1,41 @@
-import axios from "axios";
 import { SEND_WEATHER_REQUEST } from "./weatherTypes";
-import{call, takeEvery,put} from 'redux-saga/effects'
-import {receiveWeatherError, receiveWeatherResponse} from "./weatherAction"
+import { all, call, fork, put, takeEvery, takeLatest } from 'redux-saga/effects'
+import axios from "axios";
+import { receiveWeatherError, receiveWeatherResponse } from "./weatherAction";
 
-
-const getWeatherRequest=(query)=>{
-  return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=7ebcd0b29f8e07a508956a38313b3ce0`)
+const getWeatherRequest = (query)=>{
+    return axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${query}&units=metric&appid=7ebcd0b29f8e07a508956a38313b3ce0`)
 }
-function* handleGetWeather(action){
- try{
-  const  res= yield call(getWeatherRequest)
-  yield put(receiveWeatherResponse(res.data))
- } catch(error){
-  yield put(receiveWeatherError(error.message))
 
- }
+function* handleGetWeather(action){
+    try {
+        const res = yield call(getWeatherRequest , action.payload)
+        yield put(receiveWeatherResponse(res.data))
+    } catch (error) {
+        yield put(receiveWeatherError(error.message))
+    }
 }
 
 export function* watcherSaga(){
-  yield takeEvery (SEND_WEATHER_REQUEST,handleGetWeather)
+    yield takeLatest(SEND_WEATHER_REQUEST , handleGetWeather)
+}
+
+function* handleGetWeather2(action){
+    try {
+        const res = yield call(getWeatherRequest , action.payload)
+        yield put(receiveWeatherResponse(res.data))
+    } catch (error) {
+        yield put(receiveWeatherError(error.message))
+    }
+}
+
+export function* watcherSaga2(){
+    yield takeLatest(SEND_WEATHER_REQUEST , handleGetWeather2)
+}
+
+export function* rootSaga(){
+    yield all([
+        fork(watcherSaga),
+        fork(watcherSaga2),
+    ])
 }
